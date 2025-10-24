1146886d7fc650da6ab104e27b657bfa497ae984
Carmen E <carmenelisa.gabrie7@gmail.com>
2025-10-24 10:04:56 -0600

    Avance en funciones

--- src/gestionempleados/EmpleadoVentas.java
+++ src/gestionempleados/EmpleadoVentas.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package gestionempleados;
 
-/**
- *
- * @author DELL
- */
-public class EmpleadoVentas {
+
+public class EmpleadoVentas extends Empleado {
+    private double tasaComision=0.5;
+    private double[] ventasMes; //registro de ventas realizadas cada mes
+    private int mesActual =0; 
+    
+    //constructor
+    public EmpleadoVentas(int codigo, String nombre, double salarioBase, int horasTrabajadas, double tasaComision, int mesActual,double[] ventasMes){
+     super (codigo,nombre,salarioBase,horasTrabajadas);
+     this.tasaComision=tasaComision;
+     this.mesActual=0;
+     this.ventasMes = new double[12];
+    }
+    
+    public void registroVentas(double monto){
+        ventasMes[mesActual]+=monto;
+    }
+    
+    public double calcularComision(){
+       return ventasMes[mesActual]*tasaComision;
+    }
+    
+    public double calcularPagoMensual(double pago){
+        double salarioProp = super.calcularSalarioProporcional();
+        double comision = calcularComision();
+        return pago = salarioProp + comision;
+    }
+    
+    public double calcularVentasAnuales(){
+      
+        return 0;
+    }
+    
+    public String toString(){
+        return super.toString();
+        
+    }
+    
+    
+    
+   
+    
+    
+    
     
 }
