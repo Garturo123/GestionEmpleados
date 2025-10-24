ae4912a405a83add95da8ef7fe38c9a098a69274
Garturo123 <gaat1806@gmail.com>
2025-10-24 09:59:03 -0600

    commit

--- src/gestionempleados/registrarEmpleado.java
+++ src/gestionempleados/registrarEmpleado.java
@@ -2,10 +2,18 @@ package gestionempleados;
 
 import javax.swing.*;
 
+
+
 /**
  *
  * @author gaat1
  */
 public class registrarEmpleado extends JFrame{
-    
+    public registrarEmpleado(){
+        setTitle("Gestion de empleados");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+       
+    }
 }
