18d387bbd6352078386026f9c908ca1152efc721
Carmen E <carmenelisa.gabrie7@gmail.com>
2025-10-24 10:25:47 -0600

    finalizando clase

--- src/gestionempleados/EmpleadoVentas.java
+++ src/gestionempleados/EmpleadoVentas.java
@@ -4,38 +4,46 @@ package gestionempleados;
 public class EmpleadoVentas extends Empleado {
     private double tasaComision=0.5;
     private double[] ventasMes; //registro de ventas realizadas cada mes
-    private int mesActual =0; 
     
     //constructor
     public EmpleadoVentas(int codigo, String nombre, double salarioBase, int horasTrabajadas, double tasaComision, int mesActual,double[] ventasMes){
      super (codigo,nombre,salarioBase,horasTrabajadas);
      this.tasaComision=tasaComision;
-     this.mesActual=0;
      this.ventasMes = new double[12];
     }
     
-    public void registroVentas(double monto){
-        ventasMes[mesActual]+=monto;
+    public void registroVentas(int mes,double monto){
+        if (mes<0 || 12<mes){
+        ventasMes[mes]+=monto;}
     }
     
-    public double calcularComision(){
-       return ventasMes[mesActual]*tasaComision;
+    public double calcularComision(int mes){
+        if (mes<0 || 12<mes){
+       return ventasMes[mes]*tasaComision;}
+        else{
+            return 0;}
     }
     
-    public double calcularPagoMensual(double pago){
+    public double calcularPagoMensual(double pago,int mes){
         double salarioProp = super.calcularSalarioProporcional();
-        double comision = calcularComision();
+        double comision = calcularComision(mes);
         return pago = salarioProp + comision;
     }
     
-    public double calcularVentasAnuales(){
-      
-        return 0;
+    public double calcularVentasAnuales(double total){
+        total=0;
+        for (int i =0; i<ventasMes.length;i++){
+            double v = ventasMes[i];
+        total = total + v;}
+        return total;
     }
     
-    public String toString(){
-        return super.toString();
-        
+    public String toString(int mes,double total){
+        return super.toString() +
+                "\n Tasa de comisión: " +(tasaComision * 100) + "%"
+                + "\n Ventas de mes actual: " +ventasMes[mes]+
+                "\n Comision del mes actual: "+calcularComision(mes)+
+                "\n Total de ventas anuales: "+calcularVentasAnuales(total);  
     }
     
     
