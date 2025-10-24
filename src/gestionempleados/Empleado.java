67e4d87c74cd25f26f59e1bba50ea93efa5c33e1
NajmahZablah <najmahzablah@gmail.com>
2025-10-24 09:41:13 -0600

    Mensaje vacio

---
+++ src/gestionempleados/Empleado.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestionempleados;
+
+import java.util.Calendar;
+import javax.swing.ImageIcon;
+/**
+ *
+ * @author najma
+ */
+public class Empleado {
+    
+    // atributos 
+    protected int codigo;
+    protected String nombre;
+    protected Calendar fechaContrato;
+    protected double salarioBase;
+    protected int horasTrabajadas;
+    protected String rutaFoto;
+    
+    // Constructor 
+    public Empleado(int codigo, String nombre, double salarioBase, int horasTrabajadas) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.salarioBase = salarioBase;
+        this.horasTrabajadas = horasTrabajadas;
+        
+        this.fechaContrato = Calendar.getInstance();
+    }
+    
+    // Implementar getters
+    public int getCodigo() {
+        return codigo;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public Calendar getFechaContrato() {
+        return fechaContrato;
+    }
+    
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+    
+    public int getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+    
+    // Implementar setters
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+    
+    public void setHorasTrabajadas(int horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+    
+    public void setRutaFoto(String rutaFoto) {
+        this.rutaFoto = rutaFoto;
+    }
+    
+    // Métodos clave
+    public void registroHorasTrabajadas(int horas) {
+        if (horas < 0)
+            throw new IllegalArgumentException("Horas negativas no validas!");
+        this.horasTrabajadas += horas;
+    }
+    
+    public double calcularSalarioProporcional() {
+        
+        // Horas trabajadas según un mes estándar de 160 horas
+        double salarioPorHora = salarioBase/160.0;
+        double salarioProporcional = salarioPorHora * horasTrabajadas;
+        double salarioNeto = salarioProporcional * (1.0 - 0.035);
+        
+        return salarioNeto;
+    }
+    
+    public String toString() {
+        return "Coidgo: "+codigo+
+                "\nNombre: "+nombre+
+                "\nFecha de contratación: "+fechaContrato.getTime()+
+                (rutaFoto != null ? ("\nFoto " + rutaFoto) : " ");
+    }
+    
+}
